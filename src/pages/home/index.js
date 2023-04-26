fe527d30ebbc5b1d3507775d2b002cc3c2743385
xqy <reckless.xqy@gmail.com>
2023-04-26 15:28:05 +0800

    增加切换特效功能

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -10,14 +10,15 @@ import { UV_COORDS } from '../../assets/UV_COORDS'
 import meshList from '../../assets/mesh.json'
 
 
-let globalModel, webcamElement, render3D, meshIndex = 1
+let globalModel, webcamElement, render3D, mesh, geometry, scene
 
 const Home = () => {
     
     const webcam = useRef(null)
     const overlay = useRef(null)
+    const [activeMesh, setActiveMesh] = useState(0)
     
-    async function setupWebcam () {
+    const setupWebcam = async () => {
         return new Promise( ( resolve, reject ) => {
             webcamElement = webcam.current
             const navigatorAny = navigator;
@@ -37,13 +38,14 @@ const Home = () => {
                     error => reject());
             }
             else {
+                console.log('摄像头开启失败')
                 reject();
             }
         });
     }
 
     // 创建模型
-    function createModel () {
+    const createModel = async () => {
         return new Promise(async resolve => {
             await tf.setBackend('webgl')
             const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
@@ -58,7 +60,7 @@ const Home = () => {
         })
     }
     // 识别
-    async function recognition() {
+    const recognition = async () => {
         try {
             const video = webcam.current;
             const faces = await globalModel.estimateFaces(video, {
@@ -82,21 +84,47 @@ const Home = () => {
         }
     }
     
+    const addMesh = (meshIndex = 0) => {
+        // 创建 geometry，将 468 个人脸特征点按照一定的顺序(TRIANGULATION)组成三角网格，并加载 UV_COORDS
+        geometry = new BufferGeometry()
+        geometry.setIndex(TRIANGULATION)
+        geometry.setAttribute('uv', new Float32BufferAttribute(UV_COORDS.map((item, index) => index % 2 ? item : 1 - item), 2))
+        geometry.computeVertexNormals()
+
+        // 创建 material
+        const textureLoader = new TextureLoader();
+
+        const meshImg = meshList[meshIndex].src;// 材质图片地址 特效贴图地址
+
+        textureLoader.load(meshImg,texture => {
+            texture.encoding = sRGBEncoding
+            texture.anisotropy = 16
+            const material = new MeshBasicMaterial({
+                map: texture,
+                transparent: true,
+                color: new Color(0xffffff),
+                reflectivity: 0.5
+            });
+            mesh = new Mesh(geometry, material)
+            scene.add(mesh)
+        })
+    }
+    
     // 3D 贴图
     const loadScene = () => {
         // 3D场景
-        const scene = new Scene();
+        scene = new Scene();
 
         // 添加一些光照
         scene.add( new AmbientLight( 0xcccccc, 0.4 ) );
         
-        // 正交相机
-        const camera = new PerspectiveCamera( 45, 1, 0.1, 2000 );
+        // 透视相机
+        const camera = new PerspectiveCamera( 45, webcamElement.videoWidth / webcamElement.videoHeight, 0.1, 2000 );
         camera.add( new PointLight( 0xffffff, 0.8 ) );
         /*camera.position.x = webcamElement.videoWidth / 2;
         camera.position.y = - webcamElement.videoHeight / 2;
         camera.position.z = -( webcamElement.videoHeight / 2 ) / Math.tan( 45 / 2 )*/
-        camera.position.set(0, 0, webcamElement.videoHeight * 1.4)
+        camera.position.set(0, 0, webcamElement.videoHeight * 1.18)
         // const faceCenter = getFaceCenter()
         // camera.lookAt(scene.position)
         scene.add( camera );
@@ -107,34 +135,10 @@ const Home = () => {
             alpha: true
         });
 
-        // 创建 geometry，将 468 个人脸特征点按照一定的顺序(TRIANGULATION)组成三角网格，并加载 UV_COORDS
-        const geometry = new BufferGeometry()
-        geometry.setIndex(TRIANGULATION)
-        geometry.setAttribute('uv', new Float32BufferAttribute(UV_COORDS.map((item, index) => index % 2 ? item : 1 - item), 2))
-        geometry.computeVertexNormals()
+        addMesh()
         
-
-        // 创建 material
-        const textureLoader = new TextureLoader();
-        
-        // const meshImg = meshList[meshIndex].src;// 材质图片地址 特效贴图地址
-        
-        const meshImg = "https://dcdn.it120.cc/2022/12/06/795dea0b-76e9-48b8-a162-20b29d2731a7.png"
-        
-        textureLoader.load(meshImg,texture => {
-            texture.encoding = sRGBEncoding
-            texture.anisotropy = 16
-            const material = new MeshBasicMaterial({
-                map: texture,
-                transparent: true,
-                color: new Color(0xffffff),
-                reflectivity: 0.5
-            });
-            const mesh = new Mesh(geometry, material)
-            scene.add(mesh)
-        })
         // 根据 face mesh 实时更新 geometry
-        function updateGeometry (prediction) {
+        const updateGeometry = (prediction) => {
             const w = webcam.current.videoWidth
             const h = webcam.current.videoHeight
             const faceMesh = resolveMesh(prediction.scaledMesh, w, h)
@@ -142,7 +146,7 @@ const Home = () => {
             geometry.setAttribute('position', new Float32BufferAttribute(positionBuffer, 3))
             geometry.attributes.position.needsUpdate = true
         }
-        function resolveMesh (faceMesh, vw, vh) {
+        const resolveMesh = (faceMesh, vw, vh) => {
             return faceMesh.map(p => [p[0] - vw / 2, vh / 2 - p[1], -p[2]])
         }
 
@@ -155,24 +159,77 @@ const Home = () => {
         }
     }
     
+    const changeMesh = (meshIndex) => {
+        try {
+            mesh && scene.remove(mesh)
+            geometry && scene.remove(geometry);
+            
+            addMesh(meshIndex)
+
+        } catch (e) {
+            console.log('图片设置失败', e)
+        }
+        setActiveMesh(meshIndex)
+    }
+    
+    // 亮度
+    const [brightness, setBrightness] = useState(1)
+    const changeBrightness = (e) => {
+        setBrightness(e.target.value / 50)
+    }
+    
+    // 模糊度
+    const [blur ,setBlur] = useState(0)
+    const changeBlur = (e) => {
+        setBlur(e.target.value / 50)
+    }
+    
     useEffect(() => {
         setupWebcam()
     }, [])
     
     return(
         <div>
-            <div>
-                { meshList.map((item, index) => 
-                    <img src={item.src} 
-                         key={item.src}
-                         alt=""
-                    />) 
+            <div className='meshListBox'>
+                { meshList.map((item, index) =>
+                    <div className="meshList" key={item.src}>
+                        <img src={item.src}
+                             alt=""
+                             onClick={() => activeMesh !== index && changeMesh(index)}
+                             className={activeMesh === index ? 'activeMesh' : ''}
+                        />
+                        <span>{ item.name }</span>
+                    </div>)
                 }
             </div>
+            
             <div className="meshBox">
-                <video ref={ webcam } width={window.screen.width} height={window.screen.height}/>
-                <canvas ref={ overlay } width={window.screen.width} height={window.screen.height}/>
+                <video ref={ webcam } 
+                       width={window.screen.width} 
+                       height={window.screen.height}
+                       style={{filter: `brightness(${brightness}) blur(${blur}px)`}}
+                />
+                <canvas ref={ overlay } 
+                        width={window.screen.width} 
+                        height={window.screen.height}
+                />
             </div>
+
+            <form>
+                <label htmlFor="brightness">亮度</label>
+                <input type="range" 
+                       id="brightness" 
+                       onInput={(e) => changeBrightness(e)}
+                />
+                <br/>
+                <label htmlFor="blur">模糊度</label>
+                <input type="range" 
+                       id="blur"
+                       onInput={(e) => changeBlur(e)}
+                       defaultValue={0}
+                />
+            </form>
+            
            
         </div>
     )
